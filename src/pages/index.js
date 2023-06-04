f51823dc30812394bc6e644d84c3785c7a7e9fe2
Mary Ojo <maryojo3@gmail.com>
2023-06-04 21:03:25 +0100

    Responsiveness

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,33 @@
-import React, { useLayoutEffect, useRef } from "react";
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-import IntroPanel from "../components/IntroPanel";
-import SecondPanel from "../components/SecondPanel";
-import Navbar from "../components/Navbar";
-import { Box, Flex, Spacer, Image } from "@chakra-ui/react";
+import React, { useEffect, useState } from "react";
+import HorizontalScrollHome from "../components/HorizontalScrollHome";
+import VerticalScrollHome from "../components/VerticalScrollHome";
 import '../index.css';
 
-gsap.registerPlugin(ScrollTrigger);
 
 const IndexPage = () => {
-  const component = useRef();
-  const slider = useRef();
+  const [isHorizontal, setIsHorizontal] = useState(false);
 
-  useLayoutEffect(() => {
-    let ctx = gsap.context(() => {
-      let panels = gsap.utils.toArray(".panel"); //
-      gsap.to(panels, {
-        xPercent: -100 * (panels.length - 1),
-        ease: "none",
-        scrollTrigger: {
-          trigger: slider.current,
-          pin: true,
-          scrub: 1,
-          // snap: 1 / (panels.length - 1),
-          end: () => "+=" + slider.current.offsetWidth,
-        },
-      });
-    }, component);
-    return () => ctx.revert();
-  });
+  useEffect(() => {
+    const handleResize = () => {
+      setIsHorizontal(window.innerWidth > 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
-  <Box pos={'relative'}>
-    <Navbar />
-      <Box ref={component} pos={"relative"}>
-        <Box ref={slider} className="container">
-          <IntroPanel />
-          {/* <div className="panel">
-            <SecondPanel />
-          </div> */}
-          <div className="panel orange">TWO</div>
-          <div className="panel purple">THREE</div>
-        </Box>
-        <div className="lastContainer">Last Container</div>
-        <h1>Heloooooooo</h1>
-      </Box>
-    </Box>
+    <div>
+    {isHorizontal ? (
+      <HorizontalScrollHome />
+    ) : (
+      <VerticalScrollHome />
+    )}
+  </div>
   );
 };
 
